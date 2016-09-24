6073a4d5cbd80ca43b520dec5af2bc4303fb8fd9
Jay Kamat <jaygkamat@gmail.com>
2016-09-23 23:52:49 -0400

    Make dynamic input
    cyphertext awesomeness incoming

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ app.debug = True
 @app.route("/")
 def index():
     return render_template("index.html")
-    
+
 @app.route("/test")
 @app.route("/test/<index>")
 def data(index):
-	return Response({'Hello':'World'}, mimetype='application/json')
+	return Response({index:'World'}, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
