194c264368e3fa3fc2d0bbbfd8ba6f0b8adcc601
Jay Kamat <jaygkamat@gmail.com>
2016-09-25 02:27:33 -0400

    Implement auto sharting algorithm for fbass

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.debug = True
 
 def fizz_buzz(num):
     # This is a single line, so it's an atomic operation.
-    return list(map(lambda i:"FizzBuzz"[i*i%3*4:8--i**4%5] or i, range(1, num + 1)))
+    return list(map(lambda i:"FizzBuzz"[i*i%3*4:8--i**4%5] or str(i), range(1, num + 1)))
 
 @app.route("/")
 def index():
